a7b079fe29abd7ca9fad51ab6e5606c1649cea89
Nafiz <nafiz6@yahoo.com>
2019-03-22 17:30:10 +0600

    Histogram

---
+++ cmake-build-debug/CMakeFiles/Histogram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Histogram.dir/Histogram.c.o"
+  "Histogram.pdb"
+  "Histogram"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Histogram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
