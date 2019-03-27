b76489fcc436218c27384f38474de776b8cb697c
Nafiz Imtiaz Chowdhury <nafiz6@yahoo.com>
2019-03-27 22:51:12 +0600

    Code formatting

--- Histogram.c
+++ Histogram.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 #include <time.h>
 
 
@@ -21,9 +19,7 @@ int main(int argc, char *argv[]) {
 
     int32_t *file_in_memory = mmap(NULL, img_size, PROT_READ, MAP_PRIVATE, fd, 0);
 
-
     int i,j;
-
 
     long long histogram[4][33]= {0};
 
@@ -34,7 +30,6 @@ int main(int argc, char *argv[]) {
             register int32_t byte1 = file_in_memory[i*1536 + j*3];
             register int32_t byte2 = file_in_memory[i*1536 + j*3 + 1];
             register int32_t byte3 = file_in_memory[i*1536 + j*3 + 2];
-
 
             //RED
             histogram[0][((((byte1 << 4) & 0xFF0) | ((byte1>>12)& 0xF)) /128)]++;
@@ -55,7 +50,6 @@ int main(int argc, char *argv[]) {
             register int32_t byte2 = file_in_memory[(i+1)*1536 + j*3 + 1];
             register int32_t byte3 = file_in_memory[(i+1)*1536 + j*3 + 2];
 
-
             //GREEN 2
             histogram[2][((((byte1 << 4) & 0xFF0) | ((byte1>>12)  & 0xF)) /128)]++;
             histogram[2][((((byte1 >>20) & 0xFF0) | ((byte2>>4) & 0xF)) /128)]++;
@@ -68,7 +62,6 @@ int main(int argc, char *argv[]) {
             histogram[3][((((byte2 >> 16)& 0xF00) | (byte3 & 0xFF)) /128)]++;
             histogram[3][((((byte3 >> 8) & 0xF00) | ((byte3>>24)& 0xFF)) /128)]++;
         }
-
     }
 
     clock_t end = clock();
