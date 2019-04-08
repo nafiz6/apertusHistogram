b78ef3de37a94fcdb6ae2c7f7f3d573d99a6afd2
Nafiz <nafiz6@yahoo.com>
2019-04-08 22:53:55 +0600

    Error checks

--- Histogram.c
+++ Histogram.c
@@ -4,26 +4,46 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <time.h>
 
 static uint32_t img_size = 0x1200000;  //Assuming file size is exactly (12 x 4096 x 3072)/8 bytes
 static int16_t bins = 32;
 
+void errorCheck(int E, char *message)
+{
+    if (E)
+    {
+       printf("%s\n",message);
+       exit(EXIT_FAILURE);
+    }
+}
+
 int main(int argc, char *argv[]) 
 {
+    int fd;
+    struct stat s;
+    int32_t *file_in_memory;
+    size_t size;
+
     clock_t start = clock();
-    int fd = open(argv[1], O_RDONLY, S_IRUSR | S_IWUSR);
-    int32_t *file_in_memory = mmap(NULL, img_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    fd = open(argv[1], O_RDONLY, S_IRUSR);
+    errorCheck(fd < 0, "File could not be opened");    //Check if opened properly
+    fstat(fd, &s);
+    size = s.st_size;
+    errorCheck(size < img_size, "Incorrect file size");
+    file_in_memory = mmap(NULL, img_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    errorCheck(file_in_memory == MAP_FAILED, "Mapping has failed");
+
+
 
     int i, j;
-
     long long histogram[4][33]= {0};
 
     for (i=0; i<3072; i+=2) 
     {
         for (j=0; j<512; j++) // Odd Rows
-        {  
-
+        {
             //8 pixels from 12bytes
             register int32_t byte1 = file_in_memory[i*1536 + j*3];
             register int32_t byte2 = file_in_memory[i*1536 + j*3 + 1];
@@ -63,13 +83,14 @@ int main(int argc, char *argv[])
     }
 
     clock_t end = clock();
-    for (i =0;i<bins;i++)
+    for (i =0; i<bins; i++)
     {
         printf("%d %lld %lld %lld %lld\n",
                 i+1, histogram[0][i], histogram[1][i],
                 histogram[2][i], histogram[3][i]);
     }
-    printf("\nTime taken: %f",(double)(end - start) / CLOCKS_PER_SEC);
+    printf("\nTime taken: %f\n",(double)(end - start) / CLOCKS_PER_SEC);
+    close(fd);
 
     return 0;
 }
